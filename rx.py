713f90e98bb919f42300e91b3b0f36f1dd65c726
jeongchanYu <yjc133@naver.com>
2023-04-06 16:13:09 +0900

    l

--- rx.py
+++ rx.py
@@ -17,6 +17,7 @@ if __name__=='__main__':
     print(f"{ip}:{port}")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((ip, port))
     while True:
         data, addr = sock.recvfrom(1024)
