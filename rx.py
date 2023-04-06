2782b73f052a6b99e8ffb8e771259d9773f87a76
jeongchanYu <yjc133@naver.com>
2023-04-06 15:56:18 +0900

    l

--- rx.py
+++ rx.py
@@ -1,16 +1,26 @@
 import argparse
 import socket
+from requests import get
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='metric_synth.py arguments')
-    parser.add_argument('-l', '-L', '--local', type=bool, help='use local ip', default=False)
-    parser.add_argument('-p', '-P', '--port', type=str, help='use local ip', default='12356')
+    parser.add_argument('-p', '-P', '--port', type=int, help='use local ip', default=12356)
 
     args = parser.parse_args()
-    local = args.local
 
-    ip = socket.gethostbyname(socket.gethostname()) if local else socket.gethostbyname(socket.getfqdn())
+    ip = '127.0.0.1'
     port = args.port
 
     print(f"{ip}:{port}")
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
+    while True:
+        data, addr = sock.recvfrom(1024)
+
+        client_msg = "msg from client : {}".format(len(data))
+        client_ip = "client IP Addr : {}".format(addr)
+
+        print(client_msg)
+        print(client_ip)
