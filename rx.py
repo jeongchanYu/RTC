ba9f1737f0f69e271308c8f067f9894ca324e76e
jeongchanYu <yjc133@naver.com>
2023-04-06 16:55:05 +0900

    l

--- rx.py
+++ rx.py
@@ -1,29 +1,28 @@
 import argparse
 import socket
-from requests import get
-import re
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='metric_synth.py arguments')
-    parser.add_argument('-l', '-L', '--local', type=bool, help='use local ip', default=False)
     parser.add_argument('-p', '-P', '--port', type=int, help='UDP port', default=15888)
 
+    # config setup
     args = parser.parse_args()
-    local = args.local
-    ip = socket.gethostbyname(socket.gethostname()) if local else re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', get("http://ipconfig.kr").text)[0].replace('IP Address : ', '')
     port = args.port
+    fb_port = port+1
+    host_ip = socket.gethostbyname(socket.gethostname())
 
-    print(f"{ip}:{port}")
+    print(f"Host IP : {host_ip}")
+    print("Enter the remote IP : ")
+    remote_ip = input()
+    print(remote_ip)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((ip, port))
+    # socket open
+    tx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    rx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    rx_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    rx_sock.bind((host_ip, port))
+
     while True:
-        data, addr = sock.recvfrom(1024)
-
-        client_msg = "msg from client : {}".format(data.decode())
-        client_ip = "client IP Addr : {}".format(addr)
-
-        print(client_msg)
-        print(client_ip)
+        rx_data, addr = rx_sock.recvfrom(1024)
+        tx_sock.sendto(rx_data.encode(), (remote_ip, fb_port))
--- tx.py
+++ tx.py
@@ -1,21 +1,29 @@
 import argparse
 import socket
-from requests import get
-import re
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='metric_synth.py arguments')
-    parser.add_argument('-i', '-I', '--ip', type=str, help='UDP ip address', required=True)
     parser.add_argument('-p', '-P', '--port', type=int, help='UDP port', default=15888)
 
+    # config setup
     args = parser.parse_args()
-    ip = args.ip
     port = args.port
+    fb_port = port + 1
+    host_ip = socket.gethostbyname(socket.gethostname())
 
-    print(f"{ip}:{port}")
+    print(f"Host IP : {host_ip}")
+    print("Enter the remote IP : ")
+    remote_ip = input()
+    print(remote_ip)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # socket open
+    tx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    rx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    rx_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    rx_sock.bind((host_ip, fb_port))
+
     while True:
-        data = input()
-        sock.sendto(data.encode(), (ip, port))
+        rx_data, addr = rx_sock.recvfrom(1024)
+        tx_data = input()
+        tx_sock.sendto(tx_data.encode(), (remote_ip, port))
